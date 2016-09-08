d1c17b61aa0a257d64d9d905aecf5097b7f410f9
Mick McCarthy <mccarthy.mick0012@gmail.com>
2016-09-08 22:42:00 +0100

    Create PlayerController

---
+++ Assets/scripts/player/PlayerController.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	private float speed = 8.0f, maxVelocity = 4.0f;
+	[SerializeField]
+	private Rigidbody2D rb;
+	[SerializeField]
+	private Animator anim;
+
+	private void Awake () {
+	}
+
+	private void FixedUpdate () {
+		PlayerMoveKeyboard ();
+	}
+
+	private void PlayerMoveKeyboard () {
+		float force = 0.0f;
+		float velocity = Mathf.Abs (rb.velocity.x);
+
+		float h = Input.GetAxisRaw ("Horizontal");
+
+		if (h > 0) {
+			if (velocity < maxVelocity) {
+				force = speed;
+			}
+
+			Vector3 scale = transform.localScale;
+			scale.x = 1.0f;
+			transform.localScale = scale;
+			anim.SetBool ("walk", true);
+		} else if (h < 0) {
+			if (velocity < maxVelocity) {
+				force = -speed;
+			}
+
+			Vector3 scale = transform.localScale;
+			scale.x = -1.0f;
+			transform.localScale = scale;
+			anim.SetBool ("walk", true);
+		} else if (h == 0) {
+			anim.SetBool ("walk", false);
+		}
+		rb.AddForce (new Vector2 (force, 0));
+	}
+}
